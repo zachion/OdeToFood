5ddeac55926cb0c106f99b34214ec1edf453bd4e
Zach Ioannou <zioannou77@gmail.com>
2018-11-18 22:05:21 +0200

    added model and return via IActionResult

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController  
+    public class HomeController  : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from theHomeController";
+            var model = new Restaurant()
+            {
+                id = 4,
+                Name = "Zachs bagguettes"
+            };
+
+            return new ObjectResult(model);
         }
     }
 }
