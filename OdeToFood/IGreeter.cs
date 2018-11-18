ab9d2bc09742f6c6f52d2b29c77bb99d9977b4d9
Zach Ioannou <zioannou77@gmail.com>
2018-11-18 17:02:14 +0200

    using depedency injection and configuration vars

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessageOfTheDay()
+        {
+            return "Greetings";
+        }
+    }
+}
