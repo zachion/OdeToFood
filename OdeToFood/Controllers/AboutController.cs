cb7197a246c3d9e2e71b10db0d173c7c340798b5
Zach Ioannou <zioannou77@gmail.com>
2018-11-18 21:52:44 +0200

    added attribute based route

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    [Route("company/[controller]/[action]")]
     public class AboutController
     {
+
         public string Phone()
         {
             return "123412341241234";
         }
-
+        
         public string Address()
         {
             return "masahosettes 2123";
