d225c85a2dc946c92094e43ee22c1c796b266952
Zach Ioannou <zioannou77@gmail.com>
2018-11-21 23:45:52 +0200

    added EF

---
+++ OdeToFood/Data/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Models;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
