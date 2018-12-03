4c7c27f6a6cb846e9826390955c5d86a6e411eee
Zach Ioannou <zioannou77@gmail.com>
2018-12-03 12:05:09 +0200

    added greeter ViewComponent

---
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default", model);
+        }
+
+    }
+}
